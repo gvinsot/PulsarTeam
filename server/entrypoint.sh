c917fc29ffe28905669c31b17ab685770c7c9e12
Gigi <gvinsot@hotmail.com>
2026-03-01 22:01:32 +0100

    fix docker image

---
+++ server/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+# Configure git from environment variables
+if [ -n "$GIT_USER_NAME" ]; then
+  git config --global user.name "$GIT_USER_NAME"
+fi
+if [ -n "$GIT_USER_EMAIL" ]; then
+  git config --global user.email "$GIT_USER_EMAIL"
+fi
+
+exec "$@"
